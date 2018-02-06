1c5cad251b71a808092a479e0b0d8c2b02162c46
Thomas Benedict <tomtombene@gmail.com>
2018-02-06 12:32:48 -0700

    almost done

--- js/hangman.js
+++ js/hangman.js
@@ -1,5 +1,21 @@
-var words = ["thomas", "kayak","skiing"], difficulty = ["easy", "medium", "hard"];
+//Function to handle sound
+(function ($) {
+	$.extend({
+		playSound: function () {
+			return $(
+				'<audio class="sound-player" autoplay="autoplay" style="display:none;">'
+				+ '<source src="' + arguments[0] + '" />'
+				+ '<embed src="' + arguments[0] + '" hidden="true" autostart="true" loop="false"/>'
+				+ '</audio>'
+			).appendTo('body');
+		},
+		stopSound: function () {
+			$(".sound-player").remove();
+		}
+	});
+})(jQuery);
 
+//All the game options
 var game = {
 	score: 0,
 	lastLetter: "",
@@ -12,12 +28,14 @@ var game = {
 	usedWords: [],
 	currentWord: "",
 	progressWord: "",
+	audioUrl: "",
+	words: ["thomas", "kayak", "skiing", "snowboarding", "hiking", "boardgames", "snowmobile"],
 	populateUsedWords: function () {
 		this.usedWords.push(currentWord);
 	},
 	//sets a random word for the user to guess
 	getRandomWord: function () {
-		this.currentWord = words[Math.floor(Math.random() * Math.floor(words.length))].toUpperCase();
+		this.currentWord = this.words[Math.floor(Math.random() * Math.floor(this.words.length))].toUpperCase();
 	},
 	//updates the string with letters guessed
 	addLettersGuessed: function (guess) {
@@ -29,30 +47,94 @@ var game = {
 	},
 	returnDisplayableProgressWord: function () {
 		var tempProgressWord = "";
-		for( var i=0; i < this.progressWord.length; i++){
-			if(i === 0){
+		for (var i = 0; i < this.progressWord.length; i++) {
+			if (i === 0) {
 				tempProgressWord = this.progressWord.charAt(i);
-			}else{
+			} else {
 				tempProgressWord += " " + this.progressWord.charAt(i);
 			}
 		}
 		return tempProgressWord;
 	},
-	updateProgressWord: function (index,replacement) {
+	updateProgressWord: function (index, replacement) {
 		console.log("substr " + this.progressWord);
-			return this.progressWord.substr(0, index) + replacement + this.progressWord.substr(index + replacement.length);
-			// console.log("substr " + this.progressWord);
+		return this.progressWord.substr(0, index) + replacement + this.progressWord.substr(index + replacement.length);
+		// console.log("substr " + this.progressWord);
+	},
+	updateChalkBoard: function () {
+		//display everything on the chalkboard
+		$('#progressWord').text(this.returnDisplayableProgressWord());
+		$('#lettersGuessed').text(this.lettersGuessed);
+		$('#guessesRemaining').text(this.allowedGuesses - this.guessCount);
+		$('#wins').text(this.wins);
+		$('#losses').text(this.losses);
+	},
+	//Set up the game
+	setUp: function () {
+		this.lettersGuessed = "";
+		this.guessCount = 0;
+		setGuessCount();
+		this.getRandomWord();
+		this.progressWord = "";
+		for (var i = 0; i < this.currentWord.length; i++) {
+			this.progressWord += "_";
+		}
+		this.allowedGuesses = setGuessCount();
+		this.updateChalkBoard();
+	},
+	// removes the used word from the array so that it will not be picked again while playing.
+	removeUsedWord: function () {
+		this.words.splice($.inArray(this.currentWord.toLowerCase(), this.words), 1);
+		console.log(this.words);
+	},
+	checkFile: function () {
+		$.ajax({
+			url: 'audio/' + this.currentWord.toLowerCase() + '.wav',
+			error: function () {
+				//file does not exist
+				return false;
+			},
+			success: function () {
+				//file exists
+				return true;
+			}
+		});
+	},
+	winner: function () {
+		var imgSource = '<img class="win-img" src="images/' + this.currentWord.toLowerCase() + '.jpg">'
+		this.wins++;
+		this.removeUsedWord();
+		$('#displayImage').append(imgSource);
+		// console.log(this.checkFile());
+		// if(this.checkFile()){
+		// 	$.playSound('audio/' + this.currentWord.toLowerCase() + '.wav');
+		// }else{
+		// 	$.playSound('audio/default.wav');
+		// }
+		
+		if ($.inArray(this.currentWord.toLowerCase(), this.words) === -1) {
+			$.playSound('audio/default.wav');
+		} else {
+			$.playSound('audio/' + this.currentWord.toLowerCase() + '.wav');
+		}
+		this.setUp();
+	},
+	loser: function () {
+		this.losses++;
+		this.removeUsedWord();
+		this.setUp();
 	}
 }
 
 // sets the value of guesses allowed base of length of the word
 function setGuessCount() {
+	var difficultyValue = 0;
 	switch (game.difficulty) {
 		case "easy":
 			return game.currentWord.length + 10;
 			break;
 		case "medium":
-			return game.currentWord.length + 8;
+			return game.currentWord.length + 5;
 			break;
 		case "hard":
 			return game.currentWord.length + 2;
@@ -62,51 +144,25 @@ function setGuessCount() {
 	}
 }
 
-// initialize progressWord with underscores
-function setUp() {
-	//initialize
-	game.lettersGuessed = "";
-	game.guessCount = 0;
-	setGuessCount();
-	game.getRandomWord();
-	game.progressWord = "";
-	for (var i = 0; i < game.currentWord.length; i++) {
-		game.progressWord += "_";
-	}
-	game.allowedGuesses = setGuessCount();
-}
-
-// removes the used word from the array so that it will not be picked again while playing.
-function removeUsedWord() {
-	var index = words.indexOf(game.currentWord);
-	if (index > -1) {
-		words.splice(index, 1);
-	}
-}
-
 function makeAGuess(guess) {
 	//create the underscores where letters have not been guessed
 	for (var i = 0; i < game.currentWord.length; i++) {
 		if (game.currentWord.charAt(i) === guess) {
-			game.progressWord = game.updateProgressWord(i,guess);
+			game.progressWord = game.updateProgressWord(i, guess);
 		}
 	}
 	game.guessCount++;
-	
+
 	if (game.progressWord === game.currentWord) {
-		game.wins++;
-		removeUsedWord();
-		setUp();
+		game.winner();
 	}
-	if ((game.allowedGuesses - game.guessCount) === 0){
-		game.losses++;
-		removeUsedWord();
-		setUp();
+	if ((game.allowedGuesses - game.guessCount) === 0) {
+		game.loser();
 	}
 }
 
 //use this function to initialize everything
-setUp();
+game.setUp();
 
 document.onkeyup = function (event) {
 	$('#warning').hide();
@@ -116,14 +172,7 @@ document.onkeyup = function (event) {
 	if (/^[a-zA-Z]+$/.test(userGuess) && userGuess.length < 2) {
 		game.addLettersGuessed(userGuess);
 		makeAGuess(userGuess);
-
-		//display everything on the page
-		$('#progressWord').text(game.returnDisplayableProgressWord());
-		$('#lettersGuessed').text(game.lettersGuessed);
-		$('#guessesRemaining').text(game.allowedGuesses - game.guessCount);
-		$('#wins').text(game.wins);
-		$('#losses').text(game.losses);
-
+		game.updateChalkBoard();
 	} else {
 		document.querySelector("#warning").innerHTML = "Please press only letters!";
 		$('#warning').show();
@@ -132,22 +181,23 @@ document.onkeyup = function (event) {
 
 
 //Allow user to select a difficulty by pressing easy mediam or hard
-$(document).ready( function () {
+$(document).ready(function () {
 	$("#easy").on("click", function () {
 		game.difficulty = "easy";
 		$('#difficulty').text("Taking it easy");
-		setUp();
+		game.setUp();
 	});
-	
+
 	$("#medium").on("click", function () {
 		game.difficulty = "medium";
 		$('#difficulty').text("I'm okay with average.");
-		setUp();
+		game.setUp();
 	});
-	
+
 	$("#hard").on("click", function () {
 		game.difficulty = "hard";
 		$('#difficulty').text("Bring it on!!");
-		setUp();
+		game.setUp();
 	});
 });
+
